f0d4624794f4edcda1042be0ae0feae8b38a7f42
Ken Zhang <kenz@codeaurora.org>
2012-09-05 15:43:02 -0700

    msm_fb: display: wake up system around vsync
    To avoid frame drops due to power collapse,
    wake up cpu right before vsync.
    
    (cherry picked from commit 37e723cfad97be59d5a28e59220f9dcafe24ce32)
    
    Change-Id: I678071a1f642d96b4a6de2c40c155b156ce7ee61
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/msm_fb.h

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -38,7 +38,6 @@
 #include <linux/fb.h>
 #include <linux/list.h>
 #include <linux/types.h>
-
 #include <linux/msm_mdp.h>
 #ifdef CONFIG_HAS_EARLYSUSPEND
 #include <linux/earlysuspend.h>
@@ -185,6 +184,9 @@ struct msm_fb_data_type {
 	u32 writeback_state;
 	bool writeback_active_cnt;
 	int cont_splash_done;
+	void *copy_splash_buf;
+	unsigned char *copy_splash_phys;
+	void *cpu_pm_hdl;
 };
 
 struct dentry *msm_fb_get_debugfs_root(void);
